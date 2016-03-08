bb537faf208f4737cf02e3b881b2636f3a57fb80
giscafer <giscafer@outlook.com>
2016-03-09 01:27:55 +0800

    first commit

---
+++ build/webpack.base.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        // make sure to exclude 3rd party code in node_modules
+        exclude: /node_modules/
+      },
+      {
+        // edit this for additional asset file types
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url',
+        query: {
+          // inline files smaller then 10kb as base64 dataURL
+          limit: 10000,
+          // fallback to file-loader with this naming scheme
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  // vue-loader config:
+  // lint all JavaScript inside *.vue files with ESLint
+  // make sure to adjust your .eslintrc
+  vue: {
+    loaders: {
+      js: 'babel!eslint'
+    }
+  }
+}
---
+++ src/components/A.vue
@@ -0,0 +1,26 @@
+<style scoped>
+.container {
+  border: 1px solid #00f;
+}
+.red {
+  color: #f00;
+}
+</style>
+
+<template>
+  <div class="container">
+    <h2 class="red">{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+import { getMessage } from '../services/message'
+
+export default {
+  data () {
+    return {
+      msg: getMessage()
+    }
+  }
+}
+</script>
